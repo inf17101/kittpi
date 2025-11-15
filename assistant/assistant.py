2acdea76ac9f8091f178778c323cac829c091177
inf17101 <oliverk8@gmx.de>
2025-11-15 23:33:51 +0100

    Add assistant message history management

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -1,5 +1,6 @@
 from openai.types.shared import Reasoning
-from agents import Runner, Agent, ModelSettings
+from agents import Runner, Agent, ModelSettings, SQLiteSession, HostedMCPTool
+from agents.extensions.memory import EncryptedSession
 from openai.types.responses import ResponseTextDeltaEvent
 from aiomqtt import Client
 import asyncio
@@ -41,11 +42,32 @@ class SentenceStreamer:
         return [leftover] if leftover else []
 
 async def run_agent_on_incoming_instructions():
+
+    # Create encrypted SQLite session
+    underlying = SQLiteSession("user-1")
+
+    session = EncryptedSession(
+        session_id="user-1",
+        underlying_session=underlying,
+        encryption_key="secret-key",
+        ttl=120,
+    )
+
     assistant = Agent(
         name="Home Assistant",
         instructions="You are a helpful home assistant. Respond concise and friendly. No preamble or postamble. Do not lie. If you do not know, say it to the user.",
         model_settings=ModelSettings(reasoning=Reasoning(effort="minimal"), verbosity="low", max_tokens=500,),
         model="gpt-5-nano",
+        # tools=[
+        #     HostedMCPTool(
+        #         tool_config={
+        #             "type": "mcp",
+        #             "server_label": "tagesschau_news",
+        #             "server_url": "http://127.0.0.1:8001/news-mcp",
+        #             "require_approval": "never",
+        #         }
+        #     )
+        # ],
     )
 
     async with Client(broker, port=port) as client:
@@ -57,7 +79,7 @@ async def run_agent_on_incoming_instructions():
             print(f"Received user instruction: '{user_instruction}'")
 
             try:
-                result = Runner.run_streamed(assistant, input=f"{user_instruction}")
+                result = Runner.run_streamed(assistant, input=f"{user_instruction}\nOutput only in sentences optimzied for text-to-speech, no special characters only sentences end with dots.", session=session)
                 streamer = SentenceStreamer()
                 async for event in result.stream_events():
                     if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
